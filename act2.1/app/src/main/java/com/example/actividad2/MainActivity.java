bef999a418bb7dc86489c3a4a136c592717dd6fc
Dalia <a01635883@itesm.mx>
2021-04-28 20:21:07 -0500

    app 2.1

---
+++ act2.1/app/src/main/java/com/example/actividad2/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.actividad2;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int EXAMPLE_CODE = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+    public void toMeses(View v) {
+        Intent i = new Intent(this, MesesActivity.class);
+        startActivityForResult(i, EXAMPLE_CODE);
+    }
+
+}
---
+++ act2.1/app/src/main/java/com/example/actividad2/infoMes.java
@@ -0,0 +1,48 @@
+package com.example.actividad2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class infoMes extends AppCompatActivity {
+
+
+    private TextView textView;
+    TextView nombre,hobby,edad,telefono,direccion;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.info_mes);
+
+        Intent intent = getIntent();
+
+
+        //textView = findViewById(R.id.textView2);
+        nombre = findViewById(R.id.textView);
+        hobby = findViewById(R.id.textView2);
+        edad = findViewById(R.id.textView3);
+        telefono = findViewById(R.id.textView4);
+        direccion = findViewById(R.id.textView5);
+
+        //textView.setText("Hi " +intent.getStringExtra("myName"));
+        nombre.setText("Nombre: " + intent.getStringExtra("nombre"));
+        edad.setText("Edad: " + intent.getStringExtra("edad"));
+        hobby.setText("Hobby: " + intent.getStringExtra("hobby"));
+        telefono.setText("Telefono: " + intent.getStringExtra("telefono"));
+        direccion.setText("Direccion: " + intent.getStringExtra("direccion"));
+
+    }
+    public void goBack(View v){
+
+        Intent i = new Intent();
+        setResult(Activity.RESULT_OK, i);
+
+        finish();
+    }
+
+}
